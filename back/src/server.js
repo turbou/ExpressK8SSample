3f77508a3c04eecbe0287394d7e51ff4aca1b222
turbou <turbou@i.softbank.jp>
2022-12-06 22:03:26 +0900

    update.

--- back/src/server.js
+++ back/src/server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello Oyoyo');
+  res.send('Hello Contrast');
 });
 app.listen(PORT, HOST);
 console.log(`Running on http://${HOST}:${PORT}`);
